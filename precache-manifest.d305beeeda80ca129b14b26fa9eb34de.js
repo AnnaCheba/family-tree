b6497dc20e8c37dfbd2b085bb70f2290531d0811
Anna <anja.cheba@gmail.com>
2020-04-14 23:38:51 +1000

    Updates

---
+++ precache-manifest.d305beeeda80ca129b14b26fa9eb34de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccb5b482ed334eba5bd36d00ecef1f9",
+    "url": "/family-tree/index.html"
+  },
+  {
+    "revision": "eec437468579bfa3e7d1",
+    "url": "/family-tree/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dcce085e3e0c023e4726",
+    "url": "/family-tree/static/js/2.3bb5c68c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/family-tree/static/js/2.3bb5c68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec437468579bfa3e7d1",
+    "url": "/family-tree/static/js/main.69b85ac2.chunk.js"
+  },
+  {
+    "revision": "c84279e638ed882d0211",
+    "url": "/family-tree/static/js/runtime-main.0ed72196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/family-tree/static/media/logo.5d5d9eef.svg"
+  }
+]);
